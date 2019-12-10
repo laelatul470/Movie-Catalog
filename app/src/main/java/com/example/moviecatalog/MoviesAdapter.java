df5e69e133eb4beb4cafc9ca23251cc2ff3066ee
Dhimas Panji Sastra <tumbalig001@gmail.com>
2019-12-10 23:38:57 +0700

    menambahkan Model untuk Movie dan apikey

---
+++ app/src/main/java/com/example/moviecatalog/MoviesAdapter.java
@@ -0,0 +1,63 @@
+package com.example.moviecatalog;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.CardViewHolder> {
+
+    public ArrayList<Movies> listMovies = new ArrayList<>();
+
+    public void setData(ArrayList<Movies> items) {
+        listMovies.clear();
+        listMovies.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MoviesAdapter.CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+        return new CardViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesAdapter.CardViewHolder holder, int position) {
+        String urlImg = "https://image.tmdb.org/t/p/w780/";
+        Movies movies = listMovies.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(urlImg+movies.getImages())
+                .apply(new RequestOptions().override(500, 500))
+                .into(holder.imgPhoto);
+        holder.title.setText(movies.getTitle());
+        holder.deskripsi.setText(movies.getDeskripsi());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listMovies.size();
+    }
+
+    public class CardViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView title, deskripsi;
+
+        public CardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto =  itemView.findViewById(R.id.img_item);
+            title = itemView.findViewById(R.id.tv_judul);
+            deskripsi = itemView.findViewById(R.id.tv_deskripsi);
+        }
+    }
+}
